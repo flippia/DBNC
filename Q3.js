820210568ae0bea1ab9b226fcec15d4e9ef55c12
flippia <79864573+flippia@users.noreply.github.com>
2021-10-09 19:26:48 -0400

    Update Q3.js

--- Q3.js
+++ Q3.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var file = fs.readFileSync('data.txt','utf8').toString().replace(/"/g,'').split('\n')
+var file = fs.readFileSync('Q3data.txt','utf8').toString().replace(/"/g,'').split('\n')
 var headers = (file.shift()).split(',');
 let sum=0;
 for(let i = 0; i < file.length; i++) {
